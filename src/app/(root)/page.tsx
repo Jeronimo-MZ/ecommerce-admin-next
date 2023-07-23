34a8d73b281b8ddcaaf0accff1ac2bf6a6d3cbba
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-07-23 20:35:45 +0200

    feat: setup clerk

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="p-4">
-      <Button>
-        <span>Click me</span>
-      </Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
